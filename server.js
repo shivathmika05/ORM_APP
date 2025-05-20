e5cf5d0b85d36b5597519082e9403d5f06d45e8d
shivathmika05 <sivathmikanalanagula2004@gmail.com>
2025-05-20 12:50:51 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+require('./db')
+
+const app = express();
+app.use(express.json());
+
+const userRouter = require('./userroutes')
+app.use('/tasks', userRouter)
+
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
